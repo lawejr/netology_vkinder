5a897c1215742fb7e67643e4282953aa7a1505ad
lawejr <dev.lawejr@gmail.com>
2021-09-21 23:38:53 +0500

    Добавлено описание модели пользователя

--- db/models/User.py
+++ db/models/User.py
@@ -1,4 +1,5 @@
 import sqlalchemy as sq
+from sqlalchemy import CheckConstraint
 
 from .Base import Base
 
@@ -7,5 +8,27 @@ class User(Base):
     __tablename__ = 'user'
 
     id = sq.Column(sq.Integer, primary_key=True)
+    first_name = sq.Column(sq.String)
+    last_name = sq.Column(sq.String)
+    sex = sq.Column(sq.Integer, default=0)
+    birth_date = sq.Column(sq.Date)
+    status = sq.Column(sq.String)
+    home_town = sq.Column(sq.String)
 
-    pass
+    @property
+    def profile_link(self):
+        return f'https://vk.com/id{self.id}'
+
+    @property
+    def age(self):
+        if self.birth_date:
+            # TODO: текущая дата - self.birth_date
+            return 0
+        else:
+            return None
+
+    __table_args__ = (
+        CheckConstraint(0 <= sex <= 2, name='check_sex_from_0_to_2'),
+        {})
+
+
