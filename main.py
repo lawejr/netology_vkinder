d95edcba12dc6ff138119dc9ad5de008d448ab9e
lawejr <dev.lawejr@gmail.com>
2021-10-11 13:40:29 +0500

    Ключи доступа вынесены в .env

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
+import os
+from dotenv import load_dotenv
 import vk_api
+from vk_api.longpoll import VkLongPoll
+
 from VKDatingBot import VKDatingBot
 
 if __name__ == '__main__':
-    group_token = input('Введите token группы ВКонтакте: ')
-    app_token = input('Введите token с правами на поиск пользователей: ')
-    group_api = vk_api.VkApi(token=group_token)
-    app_api = vk_api.VkApi(token=app_token)
+    dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+    if os.path.exists(dotenv_path):
+        load_dotenv(dotenv_path)
+
+    APP_TOKEN = os.getenv('APP_TOKEN')
+    GROUP_TOKEN = os.getenv('GROUP_TOKEN')
+    if not APP_TOKEN:
+        raise EnvironmentError('APP_TOKEN не задан в переменных окружения')
+    if not GROUP_TOKEN:
+        raise EnvironmentError('GROUP_TOKEN не задан в переменных окружения')
+
+    app_api = vk_api.VkApi(token=APP_TOKEN)
+    group_api = vk_api.VkApi(token=GROUP_TOKEN)
+
+    # Проверка созданных клиентов api и токенов на валидность, в случае ошибки будет выброшено исключение
+    app_api.method('users.get')
+    VkLongPoll(group_api)
+    # Если проверка пройдена передаём управление боту
     VKDatingBot(group_client=group_api, app_client=app_api)
